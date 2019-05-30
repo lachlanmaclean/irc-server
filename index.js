214981f763fac55c18dd991cf68a583088d6ffed
lachlanmaclean <admin@cubecrafted.com>
2019-05-30 17:42:22 +1200

    trying to fix port binds

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ const rug = require('random-username-generator');
 const PORT = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-    console.log(PORT);
-    res.render('index.ejs', {PORT : PORT});
+    res.render('index.ejs', {port : PORT});
 });
 
 io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
+    socket.on('username', function() {
         socket.username = rug.generate();
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-        console.log(socket.username + ' is Online')
+        io.emit('is_online', ' <i>' + socket.username + ' join the chat..</i>');
+        console.log(socket.username + ' is online')
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        io.emit('is_online', '<i>' + socket.username + ' left the chat..</i>');
     });
 
     socket.on('chat_message', function(message) {
